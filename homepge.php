0001fb125f0fd5cb43aa9d3dde5c22f5a288433d
Akshat Goyal <54413209+akshatgoyal-cs@users.noreply.github.com>
2020-02-12 23:50:55 +0530

    homepage day1

---
+++ homepge.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="bootstrap3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+
+
+<div class="center">TRANSPORTATION</div>
+<div class="center1">and Logistics Solutions
+  <hr width="70%"style=color:white;>
+  </div>
+ 
+  
+
+<div style="background-color:#606060;width:100%;height:50px;" >
+        <h3 style="color:white;padding:10px;text-align:center;">Trucking<h3>
+ </div>
+ <div class="container">
+ <img src="truck.jpg" height=400 width=100% style=border-radius:8px;opacity:0.8;>
+ <div class="center1" style="color:white;text-align:center;top:10%;"><h3>Trucking and Freight Transport<h3></div>
+ <div class="center2" style="color:white;text-align:center;top:40%;">
+    <p>The world is changing all around us. To continue to thrive as a business over the next ten years and beyond, we must look ahead, understand the trends and forces that will shape our business in the future and move swiftly to prepare for what's to come. We must get ready for tomorrow today. That's what our future vision is all about. It creates a long-term destination for our business and provides us with a "Roadmap" for winning together with our partners.
+    </p>
+    </div>
+ </div>
+ 
+ 
+</body>
+</html>
